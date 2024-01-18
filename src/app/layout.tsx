ba3b3690563591b9f60af6b773b746a34c3d8cca
meet2aman <meet2amankushwaha@gmail.com>
2024-01-18 18:27:42 +0530

    gg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -21,7 +20,6 @@ export default function RootLayout({
         className={`${nunito.className} bg-gradient-to-tr from-[#0f2027] via-[#203a43] to-[#2c5364]`}
       >
         {children}
-        <SpeedInsights />
       </body>
     </html>
   );
