bca024d63a9c2daea92c6cf4fbc3f8ac1b0b854f
meet2aman <meet2amankushwaha@gmail.com>
2024-01-18 18:21:46 +0530

    some layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={`${nunito.className} bg-gradient-to-tr from-[#0f2027] via-[#203a43] to-[#2c5364]`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,80 +42,80 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={300}
+          height={80}
           priority
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left  gap-10 rounded-2xl">
+        <Link
+          href="/login"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors bg-orange-600 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-orange-700"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+          <h2 className={`mb-3 text-2xl font-bold text-white`}>
+            LogIn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[30ch] text-md opacity-50 text`}>
             Find in-depth information about Next.js features and API.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+        </Link>
+        <Link
+          href="/profile"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors bg-orange-600 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-orange-700"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+          <h2 className={`mb-3 text-2xl font-bold text-white`}>
+            Profile{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className={`m-0 max-w-[30ch] text-md opacity-50 text`}>
+            Find in-depth information about Next.js features and API.
           </p>
-        </a>
+        </Link>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        <Link
+          href="/logout"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors bg-orange-600 hover:border-gray-300 hover:bg-bg-orange-600 hover:dark:border-neutral-700 hover:dark:bg-orange-700"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+          <h2 className={`mb-3 text-2xl font-bold text-white`}>
+            Logout{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+          <p className={`m-0 max-w-[30ch] text-md opacity-50 text`}>
+            Find in-depth information about Next.js features and API.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+        </Link>
+        <Link
+          href="https://github.com/meet2aman"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors bg-black hover:border-gray-300 hover:bg-bg-orange-600 hover:dark:border-neutral-700 hover:dark:bg-slate-900"
           rel="noopener noreferrer"
+          target="_blank"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+          <h2
+            className={`mb-3 text-2xl text-center font-bold text-orange-500 uppercase`}
+          >
+            Aman Kushwaha{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p
+            className={`m-0 max-w-[30ch] text-md opacity-50 text-gray-400 text-center`}
+          >
+            A Full Stack Developer And Enginner
           </p>
-        </a>
+        </Link>
       </div>
     </main>
   );
